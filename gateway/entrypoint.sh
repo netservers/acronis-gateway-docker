2063f5419c422d18ce5eb67b49e238d790a9f183
John McEleney <john.mceleney@netservers.co.uk>
2017-04-06 09:21:00 +0100

    Add safety check to determine if Acronis is already registered.

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -20,12 +20,30 @@ if [ "$ACRONIS_UID" != "$STORE_UID" ] || [ "$ACRONIS_GID" != "$STORE_GID" ]; the
 fi
 chown -R acronis:acronis /etc/pki/tls/certs/Acronis/storage/ /var/log/Acronis/*
 
-if [ ! -e "/etc/pki/tls/certs/Acronis/storage/fes.pem" ]; then
-	if [ "$ACRONIS_HOSTNAME" == "" ]; then
-	        MYIP=`curl ifconfig.co`
-		ACRONIS_HOSTNAME=$MYIP
+if [ ! -e "/acronis_initialised" ]; then
+	if [ ! -e "/etc/pki/tls/certs/Acronis/storage/fes.pem" ]; then
+		if [ "$ACRONIS_HOSTNAME" == "" ]; then
+		        MYIP=`curl ifconfig.co`
+				ACRONIS_HOSTNAME=$MYIP
+		fi
+		acronis-storage-registration -u "$ACRONIS_USERNAME" -p "$ACRONIS_PASSWORD" -s cloud.acronis.com -a "${ACRONIS_HOSTNAME}:${ACRONIS_PORT}" -i "$ACRONIS_GATEWAYID" -o "/etc/pki/tls/certs/Acronis/storage/"
+		if [ -e "/etc/pki/tls/certs/Acronis/storage/fes.pem"]; then
+			# Success
+			date > /acronis_initialised
+		else
+			echo "Acronis was apparently not registered, so registration was attempted, and failed."
+			exit 1
+		fi
+	else
+		date > /acronis_initialised
 	fi
-	acronis-storage-registration -u "$ACRONIS_USERNAME" -p "$ACRONIS_PASSWORD" -s cloud.acronis.com -a "${ACRONIS_HOSTNAME}:${ACRONIS_PORT}" -i "$ACRONIS_GATEWAYID" -o "/etc/pki/tls/certs/Acronis/storage/"
+fi
+
+if [ ! -e "/etc/pki/tls/certs/Acronis/storage/fes.pem" ]; then
+	initialised=$(cat /acronis_initialised)
+	echo "Acronis was apparently registered on or before '$initialised', yet the certificate is missing"
+	echo "This is possibly due to a problem mounting the /etc/pki/tls/certs/Acronis/storage directory"
+	exit 1
 fi
 
 xmlstarlet ed --inplace -u "//ArchivingStorageSystemConfiguration/FrontEndServer/InternetInterface" -v "0.0.0.0:${ACRONIS_PORT}" /etc/Acronis/acronis-storage-gateway.xml
